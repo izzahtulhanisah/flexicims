8ab9a6f6ce254a53d7368b9f678d3ec2a88b896b
Unknown <syafiq.asyraf.it@gmail.com>
2018-08-28 19:32:45 +0800

    tmbh l

--- test/home.php
+++ test/home.php
@@ -82,7 +82,7 @@ while($row = $result->fetch_assoc()){
         <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <h3 class="page-header"><center>INVENTORY CONTROL</center></h3>
+                    <h3 class="page-header"><center>INVENTORY CONTROLl</center></h3>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
