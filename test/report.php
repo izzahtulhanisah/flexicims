7b2ff00e725cfb61e583d1fc8db6fa58d41eb211
Unknown <syafiq.asyraf.it@gmail.com>
2018-08-29 11:21:45 +0800

    report

--- test/report.php
+++ test/report.php
@@ -26,6 +26,29 @@ while($row = $result->fetch_assoc()){
     <?php
 	include 'include/head.php';
 	?>
+	
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+	
+	<script>
+
+	$( document ).ready(function() {
+		$('#select1').on("change", function(){
+		  var selectedClass = $(this).val(); //store the selected value
+		  $('#select2').val("");             //clear the second dropdown selected value
+
+		  //now loop through the 2nd dropdown, hide the unwanted options
+		  $('#select2 option').each(function () {
+			var newValue = $(this).attr('class');
+			if (selectedClass != newValue && selectedClass != "") {
+				$(this).hide();
+			}
+		  else{$(this).show(); }
+		 });
+
+		});
+	});
+
+	</script>
 
 </head>
 
@@ -49,15 +72,16 @@ while($row = $result->fetch_assoc()){
 					$todaydate=date("Y-m-d");
 					$before30=date('Y-m-d', strtotime("-30 days"));
 					?>
-					<!-- <div class="row">	-->						
+					<div class="row">						
 						<form action="reportform.php" method="post" name="report_card" id="form_id" target="myNewWinsr">
-							<!--<div class="row">
+							<div class="row">
 								<div class="col-lg-8">
 									<label>Activity</label>
-									<select class="form-control" id="select1" name="activity" >
+									<select class="form-control" name="activity" >
 										<option disabled selected>Select..</option>
-										<option>Incoming</option>
-										<option>Outgoing</option>
+										<option value="All">All</option>
+										<option value="Incoming">Incoming</option>
+										<option value="Outgoing">Outgoing</option>
 									</select>
 								</div>
 							</div>
@@ -66,60 +90,62 @@ while($row = $result->fetch_assoc()){
 								<br>
 							<div class="row">								
 								<div class="col-lg-4">
-									<select class="form-control" id="select2" name="location" >
+									<select class="form-control" id="select1" name="location">
 										<option disabled selected>Select Location..</option>
+										<option value ="" ></option>
 										<?php
 										
-										//$selectloc = "SELECT * FROM location";						
-										//$resultloc = $conn->query($selectloc);
-										//while($rowloc = $resultloc->fetch_assoc()){
-											//$id = $rowloc["id"];
-											//$location = $rowloc["location"];
+										$selectloc = "SELECT * FROM location";						
+										$resultloc = $conn->query($selectloc);
+										while($rowloc = $resultloc->fetch_assoc()){
+											$id = $rowloc["id"];
+											$location = $rowloc["location"];
 										
-										//echo "<option value='". $location ."'>". $location ."</option>";
+										echo "<option value='". $location ."'>". $location ."</option>";
 										
-										//}
+										}
 										
 										?>
 									</select>
 								</div>
 								<div class="col-lg-4">
-									<select class="form-control" id="select2" name="location2" >
+									<select class="form-control" id="select2" name="sublocation" >
 										<option disabled selected>Select Sub-location..</option>
+										<option value ="" ></option>
 										<?php
 										
-										//$selectloc2 = "SELECT location,location2 FROM inventory GROUP BY location,location2";						
-										//$resultloc2 = $conn->query($selectloc2);
-										//while($rowloc2 = $resultloc2->fetch_assoc()){
-										//	$id = $rowloc2["id"];
-										//	$location = $rowloc2["location"];
-										//	$location2 = $rowloc2["location2"];
+										$selectloc2 = "SELECT location,location2 FROM inventory GROUP BY location,location2";						
+										$resultloc2 = $conn->query($selectloc2);
+										while($rowloc2 = $resultloc2->fetch_assoc()){
+											$id = $rowloc2["id"];
+											$location = $rowloc2["location"];
+											$location2 = $rowloc2["location2"];
 										
-										//echo "<option class='".$location."'>". $location2 ."</option>";
+										echo "<option class='".$location."'>". $location2 ."</option>";
 										
-										//}
+										}
 										
 										?>
 									</select>
 								</div>							
 							</div>
 							<br>
-							<div class="row">
+							<!-- <div class="row">
 								<div class="col-lg-8">
 									<label>Type</label>
 									<select class="form-control" name="type" >
-										<option disabled selected>Select Sub-location..</option>
-										<?php
+										<option disabled selected>Select Type..</option>
+										<php
 										
-										//$selectloc2 = "SELECT * FROM type";						
-										//$resultloc2 = $conn->query($selectloc2);
-										//while($rowloc2 = $resultloc2->fetch_assoc()){
-										//	$id = $rowloc2["id"];
-										//	$type = $rowloc2["type"];
+										$selectloc2 = "SELECT * FROM type";						
+										$resultloc2 = $conn->query($selectloc2);
+										while($rowloc2 = $resultloc2->fetch_assoc()){
+											$id = $rowloc2["id"];
+											$type = $rowloc2["type"];
 										
-										//echo "<option>". $type ."</option>";
+										echo "<option>". $type ."</option>";
 										
-										//}
+										}
 										
 										?>
 									</select>
