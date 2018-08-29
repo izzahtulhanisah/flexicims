3e54fcfe88d43d403bfbc9330dcdab9816723c18
Unknown <syafiq.asyraf.it@gmail.com>
2018-08-29 11:29:01 +0800

    setting

--- test/setting.php
+++ test/setting.php
@@ -43,6 +43,44 @@ include 'include/menu.php';
 			<div class="col-12">
 				<center>
 					<a href="companyprofile.php" class="btn btn-danger">COMPANY PROFILE</a>
+					<?php
+					if($secpass == 2){
+					?>
+					<a href="assignstaff.php" class="btn btn-primary">ASSIGN LEADER</a>
+					<?php
+					}
+					elseif($secpass == 1){
+					?>	
+					<a href="assignmanager.php" class="btn btn-primary">ASSIGN LEADER</a>
+					<?php	
+					}
+					else{
+					?>
+					<a href="#" class="btn btn-primary" data-toggle="modal" data-placement="bottom" data-target="#myModal" title="Assign">ASSIGN LEADER</a>
+					<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+						<div class="modal-dialog">
+							<div class="modal-content">
+								<div class="modal-header">
+									<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+									<h4 class="modal-title" id="myModalLabel">Assign Staff</h4>
+								</div>
+								
+								<div class="modal-body">
+									<a href="assignmanager.php" class="btn btn-primary">ASSIGN LEADER(MANAGER)</a>
+									<a href="assignstaff.php" class="btn btn-primary">ASSIGN LEADER(STAFF)</a>
+								</div>
+								<div class="modal-footer">
+									<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+								</div>
+							</div>
+							<!-- /.modal-content -->
+						</div>
+						<!-- /.modal-dialog -->
+					</div>
+					<!-- /.modal -->
+					<?php
+					}
+					?>
 				</center>
 			<!-- /.col-lg-12 -->
 			</div>
